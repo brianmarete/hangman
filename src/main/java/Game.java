af73b7a5ae94f3a011d1596fcae70d70190c637d
Brian Marete <bmarete10@gmail.com>
2017-04-27 09:55:19 +0300

    Add member variables and constructor to Game class

---
+++ src/main/java/Game.java
@@ -0,0 +1,12 @@
+public class Game {
+  private String mMysteryWord;
+  private StringBuilder mCurrentGuess; // I used StringBuilder because it is mutable
+  private static final int MAX_GUESSES = 6;
+
+  public Game(String word) {
+    this.mMysteryWord = word;
+    this.mCurrentGuess = initializeCurrentGuess(this.mMysteryWord);
+  }
+
+
+}
