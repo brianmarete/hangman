761399515bc562b47c3c4c3249604a4aecad93b5
Brian Marete <bmarete10@gmail.com>
2017-04-27 14:07:49 +0300

    Add game running logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,48 @@
-class App {
+import java.util.Scanner;
+
+public class App {
   public static void main(String[] args){
-    System.out.println("Hangman game");
+    Scanner sc = new Scanner(System.in);
+    boolean continuePlaying = true;
+    Dictionary dictionary = new Dictionary();
+
+    while(continuePlaying) {
+      Game hangman = new Game(dictionary.getRandomWord());
+      Player player = new Player(hangman);
+
+      System.out.println("\n" + hangman.drawPicture(player) + "\n");
+
+      do {
+        System.out.println(hangman.getFormalCurrentGuess());
+
+        System.out.println(hangman.getMysteryWord());
+        System.out.println("Tries left: " + (Game.MAX_GUESSES - player.getCurrentTry()));
+        System.out.println("\nEnter a character you think is in the word");
+        char guess = (sc.next().toUpperCase()).charAt(0);
+
+        while (player.hasPreviouslyGuessed(guess)) {
+          System.out.println("\nTry again, you've already guessed that character.");
+          guess = (sc.next().toUpperCase()).charAt(0);
+        }
+
+        if (player.playGuess(guess)) {
+          System.out.println("\nCorrect!");
+        } else {
+          System.out.println("\nIncorrect!");
+        }
+
+        System.out.println("\n" + hangman.drawPicture(player) + "\n");
+      } while (!hangman.gameOver(player));
+
+      if (player.didWeWin(hangman)) {
+        System.out.println("\nCongrats! You guessed the right word! " + hangman.getMysteryWord());
+      } else if (player.didWeLose()) {
+        System.out.println("\nSorry, you lost. The word was " + hangman.getMysteryWord() + ".");
+      }
+
+      System.out.println("\nDo you want to play another game? (Y/N)");
+      char response = (sc.next().toUpperCase()).charAt(0);
+      continuePlaying = (response == 'Y');
+    }
   }
 }
