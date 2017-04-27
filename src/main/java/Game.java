10683df45d45aa9f07f5e52566f6f8eb2abd8e43
Brian Marete <bmarete10@gmail.com>
2017-04-27 10:25:15 +0300

    Add method to initialize currentGuess

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -20,4 +20,22 @@ public class Game {
   public String getFormalCurrentGuess() {
     return "Current Guess: " + this.mCurrentGuess.toString();
   }
+
+  public StringBuilder initializeCurrentGuess(String word) {
+    StringBuilder current = new StringBuilder();
+    
+    /* 
+     * The output for this method should consist of alternating
+     * underscores and spaces eg "_ _ _ _". The number of underscores
+     * should be equal to the length of the passed in word. To do this 
+     * we need to append alternating "_" and " " to the StringBuilder object
+     * using a loop that will run for twice the length of the passed in word
+     * because the output will consist of an equal number of underscores and spaces.
+     * */
+    for (int i = 0; i < word.length() * 2; i++) {
+      (i % 2 == 0) ? current.append("_") : current.append(" ");
+    }
+
+    return current;
+   }
 }
