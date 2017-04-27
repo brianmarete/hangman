3c782546592fc74b05f9bf11663ebf66eace9a9a
Brian Marete <bmarete10@gmail.com>
2017-04-27 11:36:05 +0300

    Create member variables and constructor

---
+++ src/main/java/Player.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Player {
+  private int mCurrentTry;
+  private List<Character> mPreviousGuesses;
+  private Game mGame;
+
+  public Player (Game game) {
+    this.mGame = game;
+    this.mCurrentTry = 0;
+    this.mPreviousGuesses = new ArrayList<>();
+  }
+
+}
