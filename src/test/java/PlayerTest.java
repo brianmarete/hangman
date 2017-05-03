10979aa3432a7c3ec2e7a9aa9babf2455f32b4dd
Brian Marete <bmarete10@gmail.com>
2017-05-03 12:35:23 +0300

    Add test to check previousGuesses list contains previous guesses

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -3,26 +3,31 @@ import static org.junit.Assert.*;
 
 public class PlayerTest {
 
-  Game testGame;
-  Player testPlayer;
+    Game testGame;
+    Player testPlayer;
 
-  @Before
-  public void setUp() {
-    testGame = new Game("MOVIE");
-    testPlayer = new Player(testGame);
-  }
+    @Before
+    public void setUp() {
+        testGame = new Game("MOVIE");
+        testPlayer = new Player(testGame);
+    }
 
-  @Test
-  public void getCurrentTry_itInstantiatesCorrectly_0() {
-    assertEquals(0, testPlayer.getCurrentTry());
-  }
+    @Test
+    public void getCurrentTry_itInstantiatesCorrectly_0() {
+        assertEquals(0, testPlayer.getCurrentTry());
+    }
 
-  @Test
-  public void getCurrentTry_itIncrementsAfterPlayerGuessesWrongCharacter_1() {
-    testPlayer.playGuess('Q');
-    assertEquals(1, testPlayer.getCurrentTry());
-  }
-
+    @Test
+    public void getCurrentTry_itIncrementsAfterPlayerGuessesWrongCharacter_1() {
+        testPlayer.playGuess('Q');
+        assertEquals(1, testPlayer.getCurrentTry());
+    }
+    
+    @Test
+    public void hasPreviouslyGuessed_itShouldIncludeCharactersPreviouslyGuessed_true() {
+        testPlayer.playGuess('Q');
+        assertTrue(testPlayer.hasPreviouslyGuessed('Q'));
+    }
    
       
 }
